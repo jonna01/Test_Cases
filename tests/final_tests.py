c62d53027d75117434abf300c3ebaed120e51078
jonna01 <jonnakuti.2@wright.edu>
2023-07-09 21:06:56 -0400

    Committing Changes

--- tests/final_tests.py
+++ tests/final_tests.py
@@ -1,17 +1,17 @@
 import test_main
 
-def test_validate_string_on_success():
+def test_validate_string_on_success_1():
     ## Testing String Validation with ab string
     assert test_main.string_validation_on_success("ab") == True
     
-def test_validate_string_on_success():
+def test_validate_string_on_success_2():
     ## Testing String Validation with za string
     assert test_main.string_validation_on_success("za") == True
 
-def test_validate_string_on_failure():
+def test_validate_string_on_failure_1():
     ## Testing String Validation with zx string
     assert test_main.string_validation_on_failure("zx") == True
 
-def test_validate_string_on_failure():
+def test_validate_string_on_failure_2():
     ## Testing String Validation with aab string
     assert test_main.string_validation_on_failure("aab") == True
